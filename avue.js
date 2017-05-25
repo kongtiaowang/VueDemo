17877fcfd1e147d422fa84a4720f56d07e3791f7
Wang Shen <kongtiaowangshen@gmail.com>
2017-05-25 16:00:15 -0400

    html

--- avue.js
+++ avue.js
@@ -1,16 +1,6 @@
 new Vue({
-  el: '#editor',
+  el: '#app',
   data: {
     input: '# hello'
-  },
-  computed: {
-    compiledMarkdown: function () {
-      return marked(this.input, { sanitize: true })
-    }
-  },
-  methods: {
-    update: _.debounce(function (e) {
-      this.input = e.target.value
-    }, 300)
   }
-})
+});
